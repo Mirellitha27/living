6b9febb2ec434db314d1c409c948c2fcb9931dec
MireyaJim <anamireyaj@gmail.com>
2020-10-27 15:16:39 -0600

    update loginActivity

--- app/src/main/java/com/iwebsapp/living/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/iwebsapp/living/ui/auth/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.iwebsapp.living.ui.auth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.iwebsapp.living.R
@@ -26,6 +28,13 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        val attrib = window.attributes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            attrib.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+        
         val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
         val api = MyApi(networkConnectionInterceptor)
         val db = AppDatabase(this)
@@ -37,7 +46,6 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
         binding.viewmodel = viewModel
 
         viewModel.authListener = this
-
     }
 
     override fun onStarted() {
